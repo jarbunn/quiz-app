348ce0138ba4719969d8b7feff5da0d21ac9fcae
Jacob Bunn <jarbunn18@gmail.com>
2024-11-20 04:56:42 -0700

    - Added icon - Updated image to be transparent

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -13,16 +13,18 @@ class StartScreen extends StatelessWidget {
         Image.asset(
           'assets/images/quiz-logo.png',
           width: 200,
+          color: const Color.fromARGB(150, 255, 255, 255),
         ),
         const SizedBox(height: 80),
         const Text('Learn Flutter the fun way!', style: TextStyle(color: Colors.white, fontSize: 24),),
         const SizedBox(height: 30),
-        OutlinedButton(
+        OutlinedButton.icon(
           onPressed: () {},
           style: OutlinedButton.styleFrom(
             foregroundColor: Colors.white,
           ),
-          child: const Text('Start Quiz'),
+          icon: const Icon(Icons.arrow_right_alt),
+          label: const Text('Start Quiz'),
         ),
       ]),
     );
