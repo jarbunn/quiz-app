168f92bd5bae3babdfcdd8184c88e1d38a2c415f
Jacob Bunn <jarbunn18@gmail.com>
2024-11-22 03:58:09 -0700

    Added models and data objects. Added questions and answers to quiz. Added Formatting.

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,6 +1,6 @@
-import 'dart:io';
-
+import 'package:quiz_app/answer_button.dart';
 import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget{
   const QuestionsScreen({super.key});
@@ -11,8 +11,24 @@ class QuestionsScreen extends StatefulWidget{
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+
+  final currentQuestion = questions[0];
+
   @override
   Widget build(context){
-    return Text('test');
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(currentQuestion.text, style: const TextStyle(color: Colors.white)),
+          const SizedBox(height: 30,),
+          AnswerButton(answerText: currentQuestion.answers[0], onTap: () {},),
+          AnswerButton(answerText: currentQuestion.answers[1], onTap: () {},),
+          AnswerButton(answerText: currentQuestion.answers[2], onTap: () {},),
+          AnswerButton(answerText: currentQuestion.answers[3], onTap: () {},)
+        ]
+      ),
+    );
   }
 }
