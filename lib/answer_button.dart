305d191d527dbea0635fcf4ef58eda1c1b8786fc
Jacob Bunn <jarbunn18@gmail.com>
2024-11-24 05:33:21 -0700

    Added answer navigation

--- lib/answer_button.dart
+++ lib/answer_button.dart
@@ -14,9 +14,10 @@ class AnswerButton extends StatelessWidget {
         style: ElevatedButton.styleFrom(
           padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 40),
           backgroundColor: const Color.fromARGB(255, 50, 30, 195),
+          foregroundColor: Colors.white,
           shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40))
         ),
-        child: Text(answerText),
+        child: Text(answerText, textAlign: TextAlign.center,),
     );
   }
 }
