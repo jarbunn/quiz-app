c902282145f520269c83bd061313580727db2571
Jacob Bunn <jarbunn18@gmail.com>
2024-11-26 04:19:13 -0700

    Added summary screen logic

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,9 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class ResultsScreen extends StatelessWidget {
   const ResultsScreen({super.key, required this.choosenAnswers});
   
   final List<String> choosenAnswers ;
+
+  List<Map<String, Object>> getSummary() {
+    List<Map<String, Object>> summary = [];
+    for (var i = 0; i < choosenAnswers.length; i++) {
+      summary.add({
+        'question_index': i, 
+        'question': questions[i].text, 
+        'correctAnswer': questions[0].answers[0], 
+        'user_answer': choosenAnswers[i]
+        },
+      );
+    }
+    return summary;
+  }
 
   @override
   Widget build(BuildContext context) {
